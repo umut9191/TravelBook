a76912121d247a6973311f9fe30f2fc033703efb
umut9191 <umutsurmeli91@gmail.com>
2022-01-01 17:01:45 +0300

    project created mapView defined

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
