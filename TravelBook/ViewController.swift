b5ab1c0e906374e0fc816890f3430816f4fcbd5d
umut9191 <umutsurmeli91@gmail.com>
2022-01-02 01:03:13 +0300

    accurancy for location degree         //asking user for location usage         //start taking user location codes         //delegate location method; didlocationUpdate         //every updating location of phone this method get datas in locations array         //we taking and zooming  location codes added

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController, MKMapViewDelegate{
+//this lib for reaching out user location
+import CoreLocation
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
-    
+    //this is from CoreLocation lib
+    var locationManeger = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mapView.delegate = self
+        locationManeger.delegate = self
+        //this is for accurancy for location if you chose best then phone batery will faster running out.
+        locationManeger.desiredAccuracy = kCLLocationAccuracyBest
+        //asking to user for location info
+        locationManeger.requestWhenInUseAuthorization()
+        //start taking user location;
+        locationManeger.startUpdatingLocation()
+    }
+    //delegate method;
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //every pdating location of phone this method get datas in locations array
+        
+        var location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        //we took location now we will zoom on it
+        //parameter how small zoom that much raise
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
